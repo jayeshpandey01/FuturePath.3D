47f4fbaaf384f0d554f15fb06772e7ff2e3fcc91
Jayesh Pandey <116277383+jayeshpandey01@users.noreply.github.com>
2026-05-03 02:24:49 +0530

    fix: updated vite base path for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
   return {
-    base: command === 'serve' ? '/' : '/FuturePath-3D/',
+    base: '/',
     plugins: [react()],
     resolve: {
       alias: {
